703776450d3cba055778f1c094ace96f713fa371
SherlockZhong <sherlock.z0917@gmail.com>
2014-07-17 11:53:23 +0800

    finished blurry header demo

--- demos/blurryHeader/script.js
+++ demos/blurryHeader/script.js
@@ -1,5 +1,15 @@
 (function () {
 
-  
+  window.onload = function () {
+    var header = document.getElementById('header');
+    var clonedNode = document.createElement('div');
+    clonedNode.className = 'cloned-content';
+    clonedNode.innerHTML = document.getElementById('main').innerHTML;
+    header.appendChild(clonedNode);
+  }
+
+  document.onscroll = function () {
+    document.querySelector('.cloned-content').style.marginTop = - document.body.scrollTop + 'px';
+  }
 
 })();
